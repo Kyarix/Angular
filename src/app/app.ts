6f6ddfee99ee7dd9be3dceba2b30860e5b896886
Kyarix <ashley.nanami@gmail.com>
2025-10-05 14:18:47 -0300

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,14 @@ import { Mensajes } from './mensajes/mensajes';
 import { Perfil } from './perfil/perfil';
 import { Productos } from './productos/productos';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Mensajes, Perfil, Productos, Header],
+  imports: [RouterOutlet, Mensajes, Perfil, Productos, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+plus = '-2025 Sushi al Paso.';
+}
